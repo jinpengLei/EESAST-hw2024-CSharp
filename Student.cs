0f3d58dd0c2f3592d0501fe5e5a77dca5300c331
leijinpeng <1308629473@qq.com>
2025-01-23 23:06:12 +0800

    finish student manager

--- Student.cs
+++ Student.cs
@@ -3,6 +3,7 @@ namespace StudentGradeManager;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -23,7 +24,67 @@ public interface IStudent
 
 public class Student : IStudent
 {
-    // 请仅在此处实现接口，不要在此处以外的地方进行任何修改
-    // 请尽可能周全地考虑鲁棒性
-    // 提交作业时请删除这 3 行注释
+    public string Name { get; set; }
+    public int ID { get; set; }
+    public Dictionary<string, Grade> Grades { get; }
+
+    public Student(string name, string id)
+    {
+        Name = name;
+        ID = Int32.Parse(id);
+        Grades = new Dictionary<string, Grade>();
+    }
+
+    public void AddGrade(string course, string credit, string score)
+    {
+        Grades.Add(course, new Grade(Int32.Parse(credit), Int32.Parse(score)));
+    }
+
+    public void AddGrades(List<(string course, int credit, int score)> grades)
+    {
+        foreach (var grade in grades)
+        {
+            Grades.Add(grade.course, new Grade(grade.credit, grade.score));
+        }
+    }
+
+    public void RemoveGrade(string course)
+    {
+        Grades.Remove(course);
+    }
+
+    public void RemoveGrades(List<string> courses)
+    {
+        courses.ForEach(course => RemoveGrade(course));
+    }
+    public int GetTotalCredit()
+    {
+        int totalCredit = 0;
+        foreach (var value in Grades.Values)
+        {
+            totalCredit += value.Credit;
+        }
+        return totalCredit;
+    }
+    public double GetTotalGradePoint()
+    {
+        double totalGradePoint = 0;
+        foreach (var value in Grades.Values)
+        {
+            totalGradePoint += value.GradePoint * value.Credit;
+        }
+        return totalGradePoint;
+    }
+
+    public double GetGPA()
+    {
+        return GetTotalGradePoint() / GetTotalCredit();
+    }
+
+    public override string ToString()
+    {
+        string str = $"ID: {ID}, name:{Name}, gpa:{GetGPA()}";
+        return str;
+    }
+
 }
